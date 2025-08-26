8c6795817cb8f7119cbb6fc6517184224d5c47e5
Nathan Strong <nathanstrong777@gmail.com>
2025-08-25 20:37:16 -0700

    Added debounced saving of code to local storage

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,38 @@
-use leptos::prelude::*;
+use leptos::{prelude::*, server::codee::string::FromToStringCodec};
+use leptos_use::{storage::use_local_storage, use_debounce_fn};
 
 use crate::interpreter::run_code;
 
 #[component]
+#[allow(non_snake_case)]
 pub fn App() -> impl IntoView {
-    let (code, set_code) = signal("".to_string());
+    let (read_stored, write_stored, _) =
+        use_local_storage::<String, FromToStringCodec>("code-store");
+
+    let (code, set_code) = signal(read_stored.get_untracked());
     let (output, set_output) = signal("".to_string());
+
+    // Write code to local storage 500ms after last key pressed
+    let debounce = use_debounce_fn(move || {
+        write_stored.set(code.get());
+    }, 500.0);
 
     view! {
         <div
+            style:display="flex"
+            style:flex-direction="column"
             style:width="100vw"
             style:height="100vh"
-            style:display="flex"
             style:position="absolute"
             style:top="0"
             style:left="0"
-            style:flex-direction="column"
             style:padding="5vw"
         >
             <textarea
                 class:code-input
                 on:input:target=move |ev| {
                     set_code.set(ev.target().value());
+                    debounce();
                 }
                 prop:value=code
                 style:height="70%"
@@ -49,7 +60,14 @@ pub fn App() -> impl IntoView {
             >
                 "Run Code"
             </button>
-            <pre style:height="20%" style:width="100%" style:max-height="20%" style:overflow="scroll">{output}</pre>
+            <pre
+                style:height="20%"
+                style:width="100%"
+                style:max-height="20%"
+                style:overflow="scroll"
+            >
+                {output}
+            </pre>
         </div>
     }
 }
